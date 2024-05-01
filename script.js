f0022293d79809dad22bc87f87616ff84eeb0894
Krishna <159566507+Krishna28200@users.noreply.github.com>
2024-05-01 20:59:02 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,61 +1,47 @@
-console.log("Welcome to Tic Tac Toe")
-let turn = "X"
-let isgameover = false;
+let timerDisplay = document.querySelector('.timerDisplay');
+let stopBtn = document.getElementById('stopBtn');
+let startBtn = document.getElementById('startBtn');
+let resetBtn = document.getElementById('resetBtn');
 
-// Function to change the turn
-const changeTurn = ()=>{
-    return turn === "X"? "0": "X"
-}
+let msec = 00;
+let secs = 00;
+let mins = 00;
 
-// Function to check for a win
-const checkWin = ()=>{
-    let boxtext = document.getElementsByClassName('boxtext');
-    let wins = [
-        [0, 1, 2, 5, 5, 0],
-        [3, 4, 5, 5, 15, 0],
-        [6, 7, 8, 5, 25, 0],
-        [0, 3, 6, -5, 15, 90],
-        [1, 4, 7, 5, 15, 90],
-        [2, 5, 8, 15, 15, 90],
-        [0, 4, 8, 5, 15, 45],
-        [2, 4, 6, 5, 15, 135],
-    ]
-    wins.forEach(e =>{
-        if((boxtext[e[0]].innerText === boxtext[e[1]].innerText) && (boxtext[e[2]].innerText === boxtext[e[1]].innerText) && (boxtext[e[0]].innerText !== "") ){
-            document.querySelector('.info').innerText = boxtext[e[0]].innerText + " Won"
-            isgameover = true
-            document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = "200px";
-            document.querySelector(".line").style.transform = `translate(${e[3]}vw, ${e[4]}vw) rotate(${e[5]}deg)`
-            document.querySelector(".line").style.width = "20vw";
+let timerId = null;
+
+startBtn.addEventListener('click', function(){
+    if(timerId !== null){
+        clearInterval(timerId);
+    }
+    timerId = setInterval(startTimer, 10);
+});
+
+stopBtn.addEventListener('click', function(){
+    clearInterval(timerId);
+});
+
+resetBtn.addEventListener('click', function(){
+    clearInterval(timerId);
+    timerDisplay.innerHTML = `00 : 00 : 00`;
+    msec = secs = mins = 0;
+});
+
+function startTimer(){
+    msec++;
+    if(msec == 100){
+        msec = 00;
+        secs++;
+        if(secs == 60){
+            secs = 00;
+mins++;
         }
-    })
+    }
+
+    let msecString = msec < 10 ? `0${msec}` : msec;
+    let secsString = secs < 10 ? `0${secs}` : secs;
+    let minsString = mins < 10 ? `0${mins}` : mins;
+    
+
+    timerDisplay.innerHTML = `${minsString} : ${secsString} : ${msecString}`;
+
 }
-
-let boxes = document.getElementsByClassName("box");
-Array.from(boxes).forEach(element =>{
-    let boxtext = element.querySelector('.boxtext');
-    element.addEventListener('click', ()=>{
-        if(boxtext.innerText === ''){
-            boxtext.innerText = turn;
-            turn = changeTurn();
-            checkWin();
-            if (!isgameover){
-                document.getElementsByClassName("info")[0].innerText  = "Turn for " + turn;
-            } 
-        }
-    })
-})
-
-// Add onclick listener to reset button
-reset.addEventListener('click', ()=>{
-    let boxtexts = document.querySelectorAll('.boxtext');
-    Array.from(boxtexts).forEach(element => {
-        element.innerText = ""
-    });
-    turn = "X"; 
-    isgameover = false
-    document.querySelector(".line").style.width = "0vw";
-    document.getElementsByClassName("info")[0].innerText  = "Turn for " + turn;
-    document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = "0px"
-})
-
